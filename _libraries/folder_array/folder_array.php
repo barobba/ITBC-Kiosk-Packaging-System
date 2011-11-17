8a7b1f9d8b82a6278550c6106ca766f14b4dc371
Biagio Arobba <barobba@gmail.com>
2011-11-17 05:32:02 -0700

    Added verbose
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- _libraries/folder_array/folder_array.php
+++ _libraries/folder_array/folder_array.php
@@ -1,10 +1,13 @@
 <?php
 
 function folder_array($filepath) {
+  print "Traversing directory tree:\n";
   return _folder_array_recurse($filepath, $filepath);
 }
 
 function _folder_array_recurse($filename, $filepath) {
+  
+  print $filepath."\n";
   
   $is_dir = is_dir($filepath);
   if ($is_dir) {
