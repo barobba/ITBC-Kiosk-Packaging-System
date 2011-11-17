4cd040d472fc322105ac549027fafff4694af8bc
Biagio Arobba <barobba@gmail.com>
2011-11-17 06:08:09 -0700

    Removed verbos... It's working this time.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- _libraries/folder_array/folder_array.php
+++ _libraries/folder_array/folder_array.php
@@ -1,36 +1,26 @@
 <?php
 
 function folder_array($filepath) {
-  print "Traversing directory tree:\n";
   return _folder_array_recurse($filepath, $filepath);
 }
 
 function _folder_array_recurse($filename, $filepath) {
-  
-  print $filepath."\n";
-  
   // NOTE: "IS_DIR()" WASN'T WORKING ON LINUX (OR POSSIBLY PHP VERSION).
   if (is_file($filepath)) {
     // BASIS CASE
-    print "It's a file\n";
     return $filename;
   }
   else {
     // RECURSIVE CASE
-    print "It's a directory\n";
     $folders = array();
     $folder_contents = scandir($filepath);
     foreach ($folder_contents as $sub_filename) {
-
       // Skip the . and ..
       if ($sub_filename == '.' || $sub_filename == '..') {
         continue;
       }
-      
       $folders[$sub_filename] = _folder_array_recurse($sub_filename, $filepath.'/'.$sub_filename);
-      
     }
     return $folders;
   }
-  
 }
