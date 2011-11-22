60c08570b2f90ceaa11e595d2a04f44ccfdd64d9
Biagio Arobba <barobba@gmail.com>
2011-11-22 15:34:15 -0700

    Updated the notes on picture books -> pictures.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- endpoints/picture-books/json.php
+++ endpoints/picture-books/json.php
@@ -1,4 +1,29 @@
 <?php
+/*
+
+  Picture book
+  |
+  +--> Picture entries
+       |
+       +--> Picture tagging node
+            |
+            +--> Picture media (uploaded, or reference)
+            |
+            +--> Tags
+            
+  Picture entries      -> Picture book
+  Picture entries      -> Picture tagging node 
+  Picture tagging node -> Picture media (uploaded, or reference)
+  Picture tags         -> Picture tagging node
+
+1. The first request is to get the picture book.
+2. The second request is to get the entries in the book.
+3. The third request is to get each individual picture tagging node
+4. The fourth request is to get the pictures
+5. The fifth request is to get the tags
+
+*/
+
 
 require_once('../../_config/system-settings.php');
 require_once('../../_config/config.php');
@@ -12,29 +37,64 @@ if (!isset($_REQUEST['nid'])) {
 $book_nids = explode(' ', $_REQUEST['nid']);
 foreach ($book_nids as $book_nid) {
   
+  
+  
+  
+  //
+  // 1. Retrieving the picture book information
+  //
+  
   // Get the node
   $book = latget_node($book_nid);
   
   // Merge the book data to the node
   latmerge_picture_book($book);
+
+  // Fix the audio in the picture book description (if any)
+  fix_audiotext($book->descriptionAudio);
   
   // Add the user information to the book
   latmerge_user($book);
+  
+  
+  
+  
+  //
+  // 2. Retrieving the picture book entries
+  //
   
   // Get book entries
   $picture_list = latget_picture_book_list($book_nid);
   $book->pictures = $picture_list;
   
-  // Add picture-tagging-nodes & pictures
+  
+  
+  
+  //
+  // 3. Retrieving the picture tagging node
+  //
+  
   latmerge_picture_tags($book->pictures);
+  
+  
+  
+  
+  //
+  // 4. Retrieving picture media
+  //
+  
   latmerge_picture_media($book->pictures);
   fix_pictures($book->pictures);
+
+  
+  
+  
+  //
+  // 5. Retrieving pictures tagging tags
+  //
   
   // Add terms
-  latget_terms($book->pictures);
-  
-  // Fix some of the entries
-  fix_audiotext($book->descriptionAudio);
+  //latget_terms($book->pictures);
   
   $results []= $book;
 }
